3514e8212b1c39b883ec6a5f4f6c5c41d4d0b330
unknown <saracoppola996@gmail.com>
2023-07-12 10:41:47 +0200

    stampa

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,9 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args){
+
+        System.out.println("Hello Develhope java student");
+        System.out.println("17 marzo");}
+}
+
